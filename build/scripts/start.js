c0dd0400897aa06b7c3939d1d6c3dbec3c2aa2b9
Corey Bobco <corey.bobco@gmail.com>
2018-12-16 11:59:24 -0600

    Dockerized production build

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,6 @@
 const logger = require('../lib/logger')
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 logger.info('Starting server...')
 require('../../server/main').listen(PORT, () => {
   logger.success('Server is running at http://localhost:' + PORT)
